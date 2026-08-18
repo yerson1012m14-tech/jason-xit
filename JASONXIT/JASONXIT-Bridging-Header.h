e73758527f59dcd0a534b8d4da221c790a2fbcc0
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-18 04:28:28 -0500

    Update imports in Bridging-Header
    Removed UIKit and JASONXITCore imports, added sandbox_escape and kexploit imports.

--- JASONXIT/JASONXIT-Bridging-Header.h
+++ JASONXIT/JASONXIT-Bridging-Header.h
@@ -2,7 +2,9 @@
 #define JASONXIT_Bridging_Header_h
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import "JASONXITCore.h"
+
+// Importamos el escape de sandbox y las funciones del exploit nativo
+#import "sandbox_escape.h"
+#import "kexploit/kexploit.h" // Ajusta la ruta si kexploit.h está suelto dentro de su carpeta
 
 #endif
