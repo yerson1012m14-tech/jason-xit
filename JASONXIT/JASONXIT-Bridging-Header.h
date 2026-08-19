4a609fda495bb0abb83bacb1142ef47b0b23f1c1
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-18 22:23:06 -0500

    Add sandbox_escape.h to bridging header

--- JASONXIT/JASONXIT-Bridging-Header.h
+++ JASONXIT/JASONXIT-Bridging-Header.h
@@ -5,7 +5,8 @@
 #import <UIKit/UIKit.h>
 #import "JASONXITCore.h"
 #import "kexploit.h"
+#import "sandbox_escape.h"
 #import "kexploit/vnode.h"
 #import "kexploit/kexploit_opa334.h"
 
-#endif
+#endif /* JASONXIT_Bridging_Header_h */
